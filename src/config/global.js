15ed66103d09d770430dda1d045e472b80e84767
jorozcop <maechavarriaor@gmail.com>
2023-08-09 12:26:49 -0500

    ajustes
    ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -159,7 +159,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/manipular_sistemas_de_potabilizacion.pdf',
+        download: 'downloads/921200_CF06_DU.pdf',
       },
       {
         icono: 'fas fa-download',
